06e23a3c41a3a76965f55927441d645f1ec5c5cd
IoannaStama <s3747218@student.rmit.edu.au>
2020-09-24 11:58:12 +1000

    Added project in Atmel Studio to repository

---
+++ Digital_Safe_Project/Digital_Safe_Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Digital_Safe_Project.c
+ *
+ * Created: 9/24/2020 11:57:08 AM
+ * Author : yoyos
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
